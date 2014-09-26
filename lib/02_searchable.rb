996b83a5b8c9987d31d8f9a52407dad291c7c414
Norris <norrisnakagaki@gmail.com>
2014-09-26 17:04:40 -0400

    attempting lazy where

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -7,25 +7,32 @@ module Searchable
     where_str = ""
     params.each do |key, value|
       unless value == 0
-        value = "'#{value.to_s}'" if key.to_s.to_i == 0
+        value = "'#{value.to_s}'" if value.to_s.to_i == 0
       end
-
       where_str += "#{key.to_s} = #{value} AND "
     end
     where_str = where_str[0..-6]
 
-    p where_str
-
-    result = DBConnection.execute(<<-SQL)
+    if query_stack.strip[0] == "S"
+      self.query_stack << " AND " + where_str
+    else
+      self.query_stack << (<<-SQL.chomp)
       SELECT
-        *
+      *
       FROM
-        #{self.table_name}
+      #{self.table_name}
       WHERE
-        #{where_str}
-    SQL
+      #{where_str}
+      SQL
+    end
+
+    result = DBConnection.execute(query_stack)
 
     self.parse_all(result)
+  end
+
+  def query_stack
+    @stack ||= ""
   end
 end
 
--- lib/04_associatable2.rb
+++ lib/04_associatable2.rb
@@ -7,7 +7,7 @@ module Associatable
   def has_one_through(name, through_name, source_name)
     define_method(name) do
       through_options = self.class.assoc_options[through_name]
-      p source_options = through_options.model_class.assoc_options[source_name]
+      source_options = through_options.model_class.assoc_options[source_name]
 
       through_one = through_options.model_class.where(
         :id => self.send(through_options.foreign_key))
