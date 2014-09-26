20be1c9f06dff57751b432d53701af414d412655
Norris <norrisnakagaki@gmail.com>
2014-09-26 13:43:28 -0400

    Working table entry and search

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -15,34 +15,7 @@ class SQLObject
     table[0].map { |entry| entry.to_sym }
   end
 
-  def initialize(params)
-    p "hello"
-    p params
-    params.each do |attr_name, value|
-      attr_sym = attr_name.to_sym
-      unless self.columns.include?(attr_sym)
-        raise "unknown attribute '#{attr_name}'"
-      end
-    end
-  end
-
-  # def attributes
-  #   unless instance_variable_get(:@attributes)
-  #     instance_variable_set(:@attributes, Hash.new)
-  #   end
-  #
-  #   instance_variable_get(:@attributes)
-  # end
-
   def self.finalize!
-    define_method(:attributes) do
-      unless instance_variable_get(:@attributes)
-        instance_variable_set(:@attributes, Hash.new)
-      end
-
-      instance_variable_get(:@attributes)
-    end
-
     self.columns.each do |column|
       define_method(column) do
         attributes = instance_variable_get(:@attributes)
@@ -75,38 +48,115 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |result|
+      new_result = {}
+      result.each do |key, value|
+        new_result[key.to_sym] = value
+      end
+
+      self.new(new_result)
+    end
   end
 
   def self.find(id)
-    # ...
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        ? == id
+    SQL
+
+    self.parse_all(result)[0]
   end
 
   def initialize(params = {})
-    # ...
+    @attributes = params
+    params.each do |attr_name, value|
+      attr_sym = attr_name.to_sym
+      unless self.class.columns.include?(attr_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    unless instance_variable_get(:@attributes)
+      instance_variable_set(:@attributes, Hash.new)
+    end
+
+    instance_variable_get(:@attributes)
   end
 
   def attribute_values
-    # ...
+    instance_variable_get(:@attributes).values
   end
 
   def insert
-    # ...
+    ids = DBConnection.execute(<<-SQL)
+      SELECT
+        id
+      FROM
+        #{self.class.table_name}
+    SQL
+
+    next_id = ids[-1].values[0] + 1
+    self.id = next_id
+
+    attribute_key_string = "(" + "#{self.attributes.keys.map(&:to_s)}"[1..-2] + ")"
+    attribute_key_string.gsub!("\"","\'")
+
+    question_str = "(" + (["?"] * attribute_values.count).join(",") + ")"
+
+    DBConnection.execute(<<-SQL,attribute_values)
+      INSERT INTO
+        #{self.class.table_name} #{attribute_key_string}
+      VALUES
+        #{question_str}
+    SQL
   end
 
   def update
-    # ...
+    my_id = self.id
+
+    set_str = ""
+    self.attributes.each do |key, value|
+      next if key == :id
+      unless value == 0
+        value = "'#{value.to_s}'" if key.to_s.to_i == 0
+      end
+
+      set_str += "#{key.to_s} = #{value}, "
+    end
+    set_str = set_str[0..-3]
+
+    DBConnection.execute(<<-SQL, my_id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_str}
+      WHERE
+        id = ?
+    SQL
   end
 
   def save
-    # ...
+    if self.id
+      update
+    else
+      insert
+    end
   end
 end
--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,32 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+
+    where_str = ""
+    params.each do |key, value|
+      unless value == 0
+        value = "'#{value.to_s}'" if key.to_s.to_i == 0
+      end
+
+      where_str += "#{key.to_s} = #{value} AND "
+    end
+    where_str = where_str[0..-6]
+
+    p where_str
+
+    result = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_str}
+    SQL
+
+    self.parse_all(result)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
